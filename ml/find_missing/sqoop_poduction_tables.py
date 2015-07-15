539cb5081bbffc737a62481fc5bd373f71e7141b
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2015-07-15 15:38:12 -0500

    made sqoop script delete tables before sqooping

--- ml/find_missing/sqoop_poduction_tables.py
+++ ml/find_missing/sqoop_poduction_tables.py
@@ -1,7 +1,29 @@
 
 import os
 import sys
-db = 'prod_tables'
+
+os.system("export HIVE_OPTS='-hiveconf mapreduce.job.queuename=priority'")
+
+
+db = sys.argv[1]
+langs  = sys.argv[2].split(',')
+
+
+# first delete all the tables that already exist
+delete_query = "DROP TABLE IF EXISTS %(db)s.%(table)s; "
+
+for lang in langs:
+  for s1 in ['wiki_page', 'wiki_redirect', 'wiki_langlinks']
+    for s2 in ['', '_joined']
+      table = lang+s1+s2
+      params = {'db':db, 'lang': lang}
+      print cmd
+      cmd =  """hive -e " """ + delete_query % params + """ " """
+      os.system( cmd )
+
+
+
+# sqoop tables into db
 
 page_query = """
 sqoop import                                                        \
@@ -98,12 +120,63 @@ WHERE $CONDITIONS
 '
 """
 
-table_queries =  [page_query, redirect_query, langlinks_query] #revision_query
-
-langs  = sys.argv[1].split(',')
 
 os.system("export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64")
 for lang in langs:
     d = {'db':db, 'lang':lang}
-    for table_query in table_queries:
+    for table_query in [page_query, redirect_query, langlinks_query]:
         os.system( table_query % d )
+
+
+# augment page ids with titles
+
+for lang in langs:
+    d = {'db':db, 'lang':lang}
+
+    
+    create_query = """
+    CREATE TABLE  %(db)s.%(lang)swiki_redirect_joined (
+    rd_from STRING,
+    rd_to STRING)
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY '\t'
+    STORED AS TEXTFILE;
+    set mapreduce.job.queuename=priority;
+    INSERT OVERWRITE TABLE %(db)s.%(lang)swiki_redirect_joined
+    SELECT 
+    b.page_title as rd_from,
+    a.rd_title as rd_to
+    FROM %(db)s.%(lang)swiki_redirect a JOIN %(db)s.%(lang)swiki_page b ON( a.rd_from = b.page_id);
+    """
+
+
+    cmd =  """hive -e " """ +create_query % params + """ " """
+    print (cmd)
+    os.system( cmd )
+
+
+
+    create_query = """
+    CREATE TABLE  %(db)s.%(lang)swiki_langlinks_joined (
+    ll_from STRING,
+    ll_to STRING,
+    ll_lang STRING)
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY '\t'
+    STORED AS TEXTFILE;
+    set mapreduce.job.queuename=priority;
+    INSERT OVERWRITE TABLE %(db)s.%(lang)swiki_langlinks_joined
+    SELECT 
+    b.page_title as ll_from,
+    a.ll_title as ll_to,
+    a.ll_lang as ll_lang
+    FROM %(db)s.%(lang)swiki_langlinks a JOIN %(db)s.%(lang)swiki_page b ON( a.ll_from = b.page_id);
+    """
+
+
+    cmd =  """hive -e " """ +create_query % params + """ " """
+    print (cmd)
+    os.system( cmd )
+
+
+
