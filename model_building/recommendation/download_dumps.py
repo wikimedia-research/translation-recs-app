a3087b17f0254d5f9b5d31fc92d35d07d63508e9
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2015-07-15 20:43:00 -0500

    adding script for downloading dumps

--- model_building/recommendation/download_dumps.py
+++ model_building/recommendation/download_dumps.py
@@ -7,4 +7,4 @@ for lang in langs:
 	url = 'https://dumps.wikimedia.org/%(lang)swiki/latest/%(lang)s%(ftype)s' % {'lang': lang, 'ftype':ftype}
 	dest = '/user/ellery/translation-recs-app/data/%s' % lang
 	os.system('hadoop fs -mkdir %s' % dest)
-	os.system("wget -O - %(url)s | bunzip2 | hadoop fs -put %(dest)s/%(lang)s%(ftype)s" % {'url': url, 'ftype':ftype, 'dest': dest, 'lang': lang})
+	os.system("wget -O - %(url)s | bunzip2 | hadoop fs -put - %(dest)s/%(lang)s%(ftype)s" % {'url': url, 'ftype':ftype, 'dest': dest, 'lang': lang})
