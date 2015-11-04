c58a69cb91dedc4df467c29b63095618a8584388
Ellery Wulczyn <ellerywulczyn@Ellery-Wulczyns-MacBook-Pro.local>
2015-11-03 17:05:19 -0800

    adding CORS headers

--- api/recommender_api.py
+++ api/recommender_api.py
@@ -82,6 +82,12 @@ def seed_recommendations():
 
     return json_response(ret)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
 
 parser = argparse.ArgumentParser()
 
