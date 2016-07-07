7b8b0cb9abfa1d489953fe53c44fa3036d18d6c8
Ellery Wulczyn <ellerywulczyn@ElleryWczynsMBP.corp.wikimedia.org>
2016-07-07 14:26:06 -0700

    removed unused files and refactored api

--- labs_setup.sh
+++ labs_setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3 python3-dev build-essentials libatlas-base-dev gfortran
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3.4
-sudo python3 -m pip install uwsgi flask scipy pandas scikit-learn
+sudo python3 -m pip install uwsgi flask requests futures numpy
 
 sudo apt-get install nginx
